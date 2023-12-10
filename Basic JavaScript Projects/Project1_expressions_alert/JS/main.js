f73b50b7765554e58bf33689cbf1ad8a011c77a7
Jiaha0-Zhang <xihd2017@gmail.com>
2023-12-09 17:14:26 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+window.alert("Hello,World!")
+document.write("Hello,Kitty!")
+
+var X = "This is a practise.";
+document.write(X);
+
+var Y = "This is a challenge assignment.";
+window.alert(Y);
+
+document.write("Tom said, \"Don\'t you want to leave?\".");
+document.write("Jerry said,\"I don\'t want "+" to leave!\"");
+
+var Z = "Ap"+"ple"; //concatenate Ap and ple together to be varible Z
+document.write(Z); //display varible Z
+
+var Apple = "Green", Banana = "Yellow", Strawberry = "Red", Grape = "Purple"; // Define multiple variables at the same time
+document.write(Apple); //display varible Apple
+
+var Sent1 = "This is the beginning of the string"; //This is variable sent1
+var Sent2 = " and this is the end of the string"; //This is variable sen2
+document.write(Sent1+Sent2); //Concatenate the Sent1 and Sent2 variables together
+
